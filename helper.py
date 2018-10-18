697b7aebf3cb7f209b66e041fbd712852f561bb6
rava-dosa <apoorvakumar169@gmail.com>
2018-10-19 05:01:48 +0530

    added caching support and refactored code

--- helper.py
+++ helper.py
@@ -1,3 +1,92 @@
+import re
+import model
+# import 
+def main1(lis,request):
+	p1=None
+	p11=None
+	p3=None
+	p4=None
+	p5=None
+	p6=None
+	try:
+		p1 = request.args.get('keyword')
+	except:
+		pass
+	try:
+		p11 = request.args.get('sortby')
+	except:
+		pass
+		#p11 = 0 sort by date time
+		#p11 = 1 sort by tweet text.
+	try:	
+		p3 = request.args.get('flag')       
+	except:
+		pass	
+		# flag = 0 if search keyword is for tweet
+		# flag = 1 if search keyword is for username 
+		# default tweet text
+	try:
+		p4 = request.args.get('filter')
+		if p4 is not None:
+			p5 = literal_eval(p4)
+		else:
+			pass
+	except:
+		pass
+	try:
+		p6 = request.args.get('file')
+	except:
+		pass
+
+	if p1 is not None:
+		if p3 is None or int(p3)==0:
+			lis=model.searchtweet(lis,p1)
+		else:
+			lis=model.searchuser(lis,p1)
+		# sort by 
+		if p11 is not None:
+			if int(p11)==0:
+				lis=sorted(lis, key=itemgetter(1))
+			else:
+				lis=sorted(lis, key=itemgetter(7))
+		else:
+			pass
+		# all these(112-144) variables help in producing filter over the queried result
+		if p4 is not None:
+			var1,var2 = None,None
+			for x in p5:
+				try:
+					var1 = x["var1"]
+				except:
+					pass
+				try:
+					var2 = x["var2"]
+				except:
+					pass
+				# pdb.set_trace()
+				if var1 is not None:
+					if "lt" in x:
+						lis=funcoo(lis,x["lt"],var1,0)
+					elif "gt" in x:
+						lis=funcoo(lis,x["gt"],var1,1)
+					elif "et" in x:
+						lis=funcoo(lis,x["et"],var1,2)
+					else:
+						pass
+
+				if var2 is not None:
+					if "sw" in x:
+						lis=funcoo1(lis,x["sw"],var2,0)
+					elif "ew" in x:
+						lis=funcoo1(lis,x["ew"],var2,1)
+					elif "contains" in x:
+						lis=funcoo1(lis,x["contains"],var2,2)
+					elif "exact" in x:
+						lis=funcoo1(lis,x["exact"],var2,3)
+					else:
+						pass
+	return lis
+
 def f0(lis,lis1,index,val):
 	for x in lis:
 		if x[index]<val:
@@ -175,3 +264,64 @@ def funcoo1(lis,val,var2,flag):
 			lis1=f13(lis,lis1,3,val)
 
 	return lis1
+
+def urltokey(str1):
+	try:
+		str1=re.sub('&pagination=\d+', '', str1)
+	except:
+		pass
+	try:
+		str1=re.sub('&capacity=\d+', '', str1)
+	except:
+		pass
+	return str1
+
+def pagination(lis,request):
+	p2=None
+	p22=None
+	try:
+		# pdb.set_trace()
+		p2 = request.args.get('pagination')
+	except:
+		pass
+
+	try:	
+		p22 = request.args.get('capacity')
+	except:
+		pass
+
+	# pagination 
+	if p2 is not None:
+		if p22 is not None:
+			# pdb.set_trace()
+			pag=int(p2)
+			cap=int(p22)
+			currcap=len(lis)-pag*cap
+			if pag*cap < len(lis):
+				if currcap > cap:
+					lis=lis[pag*cap:pag*cap+cap]
+				else:
+					lis=lis[pag*cap:len(lis)]
+			else:
+				return("Bad request")
+		else:
+			cap=5
+			pag=int(p2)
+			currcap=len(lis)-pag*cap
+			if pag*cap < len(lis):
+				if currcap > cap:
+					lis=lis[pag*cap:pag*cap+cap]
+				else:
+					lis=lis[pag*cap:len(lis)]
+			else:
+				return("Bad request")
+
+	return lis
+
+def cach(lis,keyforcache,request,cache,time):
+	lis = cache.get(keyforcache)
+	if lis is None:
+		lis=[]
+		lis = main1(lis,request)
+		cache.set(keyforcache, lis, timeout=time)
+	return lis
--- main.py
+++ main.py
@@ -10,11 +10,15 @@ import helper
 from flask import Flask
 from flask import json
 from flask import request
+from werkzeug.contrib.cache import SimpleCache
+
 # import json
 from ast import literal_eval
 from operator import itemgetter
 
 app = Flask(__name__)
+cache = SimpleCache()
+
 
 @app.route('/')
 def hello_world():
@@ -43,131 +47,17 @@ def hello_world():
 	)
 	return response
 
+
+
 @app.route('/search')
 def summary():
-	data = {'a':100,'b':200}
-	conn = sqlite3.connect('test.db')
-	lis=[]
-	p1=None
-	p11=None
-	p2=None
-	p22=None
-	p3=None
-	p4=None
-	p5=None
-	p6=None
-	try:
-		p1 = request.args.get('keyword')
-	except:
-		pass
-	try:
-		p11 = request.args.get('sortby')
-	except:
-		pass
-		#p11 = 0 sort by date time
-		#p11 = 1 sort by tweet text.
-	try:
-		# pdb.set_trace()
-		p2 = request.args.get('pagination')
-	except:
-		pass
-	try:	
-		p22 = request.args.get('capacity')
-	except:
-		pass
-	try:	
-		p3 = request.args.get('flag')       
-	except:
-		pass	
-		# flag = 0 if search keyword is for tweet
-		# flag = 1 if search keyword is for username 
-		# default tweet text
-	try:
-		p4 = request.args.get('filter')
-		if p4 is not None:
-			p5 = literal_eval(p4)
-		else:
-			pass
-	except:
-		pass
-	try:
-		p6 = request.args.get('file')
-	except:
-		pass
+	pdb.set_trace()
+	keyforcache=helper.urltokey(request.url)
+	time=5*60
+	lis=None
+	lis=helper.cach(lis,keyforcache,request,cache,time)
+	lis=helper.pagination(lis,request)
 
-	if p1 is not None:
-		if p3 is None or int(p3)==0:
-			lis=model.searchtweet(lis,p1)
-		else:
-			lis=model.searchuser(lis,p1)
-		# sort by 
-		if p11 is not None:
-			if int(p11)==0:
-				lis=sorted(lis, key=itemgetter(1))
-			else:
-				lis=sorted(lis, key=itemgetter(7))
-		else:
-			pass
-		# all these(112-144) variables help in producing filter over the queried result
-		if p4 is not None:
-			var1,var2 = None,None
-			for x in p5:
-				try:
-					var1 = x["var1"]
-				except:
-					pass
-				try:
-					var2 = x["var2"]
-				except:
-					pass
-				# pdb.set_trace()
-				if var1 is not None:
-					if "lt" in x:
-						lis=helper.funcoo(lis,x["lt"],var1,0)
-					elif "gt" in x:
-						lis=helper.funcoo(lis,x["gt"],var1,1)
-					elif "et" in x:
-						lis=helper.funcoo(lis,x["et"],var1,2)
-					else:
-						pass
-
-				if var2 is not None:
-					if "sw" in x:
-						lis=helper.funcoo1(lis,x["sw"],var2,0)
-					elif "ew" in x:
-						lis=helper.funcoo1(lis,x["ew"],var2,1)
-					elif "contains" in x:
-						lis=helper.funcoo1(lis,x["contains"],var2,2)
-					elif "exact" in x:
-						lis=helper.funcoo1(lis,x["exact"],var2,3)
-					else:
-						pass
-	# pagination 
-	if p2 is not None:
-		if p22 is not None:
-			# pdb.set_trace()
-			pag=int(p2)
-			cap=int(p22)
-			currcap=len(lis)-pag*cap
-			if pag*cap < len(lis):
-				if currcap > cap:
-					lis=lis[pag*cap:pag*cap+cap]
-				else:
-					lis=lis[pag*cap:len(lis)]
-			else:
-				return("Bad request")
-		else:
-			cap=5
-			pag=int(p2)
-			currcap=len(lis)-pag*cap
-			if pag*cap < len(lis):
-				if currcap > cap:
-					lis=lis[pag*cap:pag*cap+cap]
-				else:
-					lis=lis[pag*cap:len(lis)]
-			else:
-				return("Bad request")
-	
 	# response
 	response = app.response_class(
 		response=json.dumps(lis),
